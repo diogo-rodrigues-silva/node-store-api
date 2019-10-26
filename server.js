c390ed42c14fd66b8b5cce7d761dcdf8aa80cde9
diogo-rodrigues-silva <diogorodrigues0801@gmail.com>
2019-10-26 15:07:24 -0300

    Add a function to normalize port value

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const debug = require('debug')('nodestore:server');
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
@@ -21,3 +21,17 @@ app.use('/', route);
 
 server.listen(port);
 console.log('API rodando na porta ' + port);
+
+function normalizePort(value) {
+    const port = parseInt(value, 10);
+
+    if (isNaN(port)){
+        return value;
+    }
+
+    if(port >=0){
+        return port;
+    }
+
+    return false;
+}
