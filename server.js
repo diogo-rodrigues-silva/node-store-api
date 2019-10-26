8bc65393822f0081dd591b8e6687588488295baf
diogo-rodrigues-silva <diogorodrigues0801@gmail.com>
2019-10-26 15:37:38 -0300

    Create function onListening and add debug

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use('/', route);
 
 server.listen(port);
 server.on('error', onError);
+server.on('listening', onListening);
 
 console.log('API rodando na porta ' + port);
 
@@ -57,4 +58,10 @@ function onError(error){
         default:
             throw error;
     }
+}
+
+function onListening(){
+    const addr = server.address();
+    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    debug('Listening on ' + bind); 
 }
